dae684c4301850618802df87044f67a820a54621
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-12-11 04:07:00 +0300

    Refactor to separate request and data processing

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,43 +1,25 @@
 const BASE_URL = "http://api.openweathermap.org/data/2.5/weather?";
 const APP_ID = "4347f0deab7bf2bc00588ae06c3ea1b0";
 
-const getWeatherByCityName = (city, units, action) => {
-  fetch(`${BASE_URL}q=${city}&appid=${APP_ID}&units=${units}`, {
+const getWeatherByCityName = (city, units) => {
+  return fetch(`${BASE_URL}q=${city}&appid=${APP_ID}&units=${units}`, {
     mode: "cors",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      action(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  }).then((res) => res.json());
 };
 
-export const getWeatherByCoord = (lat, lon, units, action) => {
-  fetch(`${BASE_URL}lat=${lat}&lon=${lon}&appid=${APP_ID}&units=${units}`, {
-    mode: "cors",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      action(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+export const getWeatherByCoord = (lat, lon, units) => {
+  return fetch(
+    `${BASE_URL}lat=${lat}&lon=${lon}&appid=${APP_ID}&units=${units}`,
+    {
+      mode: "cors",
+    }
+  ).then((res) => res.json());
 };
 
-export const getWeatherByCityId = (city_id, units, action) => {
-  fetch(`${BASE_URL}id=${city_id}&appid=${APP_ID}&units=${units}`, {
+export const getWeatherByCityId = (city_id, units) => {
+  return fetch(`${BASE_URL}id =${city_id}&appid=${APP_ID}&units=${units}`, {
     mode: "cors",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      action(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  }).then((res) => res.json());
 };
 
 export default getWeatherByCityName;
--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ import extractWeatherData from "./processData";
 
 const container = document.getElementById("container");
 
-getWeatherByCityName("kosovo", "metric", (data) => {
-  container.innerText = data.main.temp;
+const getWeather = getWeatherByCityName("kampala", "metric");
+getWeather.then((response) => {
+  const { condition } = extractWeatherData(response);
+  container.innerText = condition;
 });
 
-getWeatherByCoord(0.3476, 32.5825, "metric", (data) => {
-  const weather = document.createElement("h3");
-  weather.innerText = data.main.temp;
-  container.appendChild(weather);
-});
+// getWeatherByCoord(0.3476, 32.5825, "metric", (data) => {
+//   const weather = document.createElement("h3");
+//   weather.innerText = data.main.temp;
+//   container.appendChild(weather);
+// });
 
-getWeatherByCityId(443339, "metric", (data) => {
-  console.log(extractWeatherData(data));
-});
+// getWeatherByCityId(443339, "metric", (data) => {
+//   console.log(extractWeatherData(data));
+// });
